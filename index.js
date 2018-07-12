760dcf61000501edcdd8bd80f865f050c822e35d
fiolam <fiola.mulliqi@wework.com>
2018-07-12 01:10:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /* Enter the code to remove the main node element under this comment */
 
+var main = document.getElementById('main')
+main.remove();
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement('h1');
+newHeader.innerText = "Fiola is the champion!";
+newHeader.setAttribute("id", "victory")
+// next: find the body element
+var body = document.querySelector('body')
+// next: append the newHeader to the body
+body.appendChild(newHeader)
