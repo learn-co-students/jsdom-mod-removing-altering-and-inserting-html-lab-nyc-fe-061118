fefd9153ac2b2369cb55a08ffd9d6fb09c758afa
Hans Enlin <hans.enlin@icloud.com>
2018-07-12 00:34:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 /* Enter the code to remove the main node element under this comment */
 
+document.body.removeChild(document.querySelector('main'));
+
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader = document.createElement('h1')
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerText = 'Hans is the champion';
+
+document.body.appendChild(newHeader);
+
+
